87a2aa96643d63c179c505e1c008c8dea79b0f7d
Ul1ra <benpcunningham@outlook.com>
2018-01-11 16:43:45 +0100

    Commented a try clearing the form

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function() { // Now the code will be executed
 // Select size
   $('#sizePicker').submit(function makeGrid(grid) {
-    var rows = $('#input_height').val();
+    var rows = $('#input_height').val(); 
     var cols = $('#input_width').val();
       for (var i = 1; i <= rows; i++) {
         $('table').append("<tr></tr>");
@@ -12,10 +12,17 @@ $(document).ready(function() {
       }
       grid.preventDefault();
   });
-// Select color
+// Select color ... error, covers entire grid
   $('#pixel_canvas').click('.cell', function() {
     var paint = $('#colorPicker').val();
     $('#pixel_canvas').css('background-color', paint);
   }); 
+// Trying to add a clear option if any sell has color in it
+/*   $('form').click( function() {
+    $('#sizePicker').find('input:submit').val();
+      if ('td' == css(background-color)) {
+        $remove(css(background-color));
+      }
+  }); */
 
 });
