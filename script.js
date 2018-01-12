f8cb75a7654e7a75fbf91f3c9e1d175f4739670e
Ul1ra <benpcunningham@outlook.com>
2018-01-12 12:05:24 +0100

    fixed everything

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-$(document).ready(function() { // Now the code will be executed
-// Select size
-  $('#sizePicker').submit(function makeGrid(grid) {
-    var rows = $('#input_height').val(); 
-    var cols = $('#input_width').val();
-      for (var i = 1; i <= rows; i++) {
-        $('table').append("<tr></tr>");
-        for (var j = 1; j <= cols; j++) {
-          $('tr:last').append("<td></td>");
-          $('td').attr("class", 'cell');
-        }
-      }
-      grid.preventDefault();
-  });
-// Select color ... error, covers entire grid
-  $('#pixel_canvas').click('.cell', function() {
-    var paint = $('#colorPicker').val();
-    $('#pixel_canvas').css('background-color', paint);
-  }); 
-// Trying to add a clear option if any sell has color in it
-/*   $('form').click( function() {
-    $('#sizePicker').find('input:submit').val();
-      if ('td' == css(background-color)) {
-        $remove(css(background-color));
-      }
-  }); */
+$(document).ready(function () { // Now the code will be executed
 
+  $('#sizePicker').submit(function makeGrid(grid) { // The selector #sizePicker
+    $('table tr').remove();
+    var rows = $('#input_height').val();
+    var cols = $('#input_width').val();
+    for (var i = 1; i <= rows; i++) {
+      $('table').append("<tr></tr>");
+      for (var j = 1; j <= cols; j++) {
+        $('tr:last').append("<td></td>");
+        $('td').attr("class", 'cells')
+      }
+    }
+    grid.preventDefault();
+
+    $('.cells').click(function (event) {
+      var paint = $('#colorPicker').val();
+      $(event.target).css('background-color', paint);
+    });
+  });
 });
