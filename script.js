9d1f9a1f9264da8082382a83ffc610fe4b8774b1
Ul1ra <benpcunningham@outlook.com>
2018-01-12 13:36:18 +0100

    started adding comments

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () { // Now the code will be executed
 
-  $('#sizePicker').submit(function makeGrid(grid) { // The selector #sizePicker
-    $('table tr').remove();
+  $('#sizePicker').submit(function makeGrid(grid) { // Clicking on submit creates a single grid
+    $('table tr').remove(); // Clicking on Submit again removes the colored squares
     var rows = $('#input_height').val();
     var cols = $('#input_width').val();
     for (var i = 1; i <= rows; i++) {
-      $('table').append("<tr></tr>");
+      $('table').append("<tr></tr>"); // The for loop creates a row of pixels multiplied by the input value of heights
       for (var j = 1; j <= cols; j++) {
-        $('tr:last').append("<td></td>");
+        $('tr:last').append("<td></td>"); // This for loop creates the 
         $('td').attr("class", 'cells')
       }
     }
