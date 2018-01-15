0ed659a133bd31392f5c2abc15904f329c664314
Ul1ra <benpcunningham@outlook.com>
2018-01-15 11:58:15 +0100

    Added the needed comments

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-$(document).ready(function () { // Now the code will be executed
-
-  $('#sizePicker').submit(function makeGrid(grid) { // Clicking on submit creates a single grid
-    $('table tr').remove(); // Clicking on Submit again removes the colored squares
-    var rows = $('#input_height').val();
-    var cols = $('#input_width').val();
-    for (var i = 1; i <= rows; i++) {
-      $('table').append("<tr></tr>"); // The for loop creates a row of pixels multiplied by the input value of heights
+$(document).ready(function () { // Makes it possible to start to manipulate the document
+  $('#sizePicker').submit(function makeGrid(grid) {  // Creates the grid upon clicking the button 'Submit'
+    $('table tr').remove(); // Lets the grid be cleared when hitting the 'Submit' button again
+    var rows = $('#input_height').val(); // Allows the user to add a chosen value inside the input box to add rows 
+    var cols = $('#input_width').val(); // Allows the user to add a chosen value inside the 2nd input box to add columns
+    for (var i = 1; i <= rows; i++) { 
+      $('table').append("<tr></tr>"); // This loop creates a row of cells creating a row
       for (var j = 1; j <= cols; j++) {
-        $('tr:last').append("<td></td>"); // This for loop creates the 
-        $('td').attr("class", 'cells')
+        $('tr:last').append("<td></td>"); // This loop adds a cell after every row creating a column
+        $('td').attr("class", 'cells') // for every 'td' created, a class of 'cells' is created
       }
     }
-    grid.preventDefault();
+    grid.preventDefault(); // Prevents the grid to be deleted after it is created
 
-    $('.cells').click(function (event) {
+    $('.cells').click(function (event) { // The function allows the user to color a cell on click
       var paint = $('#colorPicker').val();
-      $(event.target).css('background-color', paint);
+      $(event.target).css('background-color', paint); // Lets the chosen color on a click event to be added to the grid
     });
   });
 });
